cd44e4234068d4f59c91ba4bbc1b6c095aa2add6
SparkSages <brendan.dalhover@gmail.com>
2023-09-12 12:54:52 -0400

    Initializing project

---
+++ Playlist Manager/Classes/Genre.cs
@@ -0,0 +1,11 @@
+namespace Classes
+{
+    public enum Genre
+    {
+        Rock,
+        Pop,
+        Dance,
+        HipHop,
+        Other
+    }
+}
---
+++ Playlist Manager/Classes/Song.cs
@@ -0,0 +1,14 @@
+using System.Net.Http.Headers;
+
+namespace Classes
+{
+
+    public class Song
+    {
+        public string Title { get; protected set; }
+        public string Artist { get; protected set; }
+        public TimeSpan duraction { get; protected set; } = new TimeSpan(0, 0, 0);
+        public Genre Genre { get; protected set; } = Genre.Other;
+    }
+
+}
